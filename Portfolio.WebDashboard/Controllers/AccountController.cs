98325b60d7a5e585d6b0a3ce45ed880a12950644
Gio García <giovannigarcia.de@hotmail.com>
2023-03-23 23:15:59 -0500

    Added Account controller, view Login

---
+++ Portfolio.WebDashboard/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.WebDashboard.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
